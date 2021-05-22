ff08dafbe5a537bb8e81cfe3255e7b62953eb459
Daniel Aprillio <daprillio@student.ciputra.ac.id>
2021-05-22 10:28:15 +0700

    comment

--- app/src/main/java/com/snap/eonmemory/MainActivity.java
+++ app/src/main/java/com/snap/eonmemory/MainActivity.java
@@ -11,8 +11,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //First Comment
-        //Welcome Back, Bryan!
+        //Good Luck!
 
     }
 }
