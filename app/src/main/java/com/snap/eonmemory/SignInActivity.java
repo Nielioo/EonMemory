1aef69c02cce435e0ec7ccd88887888acb455d06
Daniel Aprillio <daprillio@student.ciputra.ac.id>
2021-05-26 17:54:32 +0700

    SignInActivity modification

--- app/src/main/java/com/snap/eonmemory/SignInActivity.java
+++ app/src/main/java/com/snap/eonmemory/SignInActivity.java
@@ -1,12 +1,12 @@
 package com.snap.eonmemory;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.textfield.TextInputLayout;
 
@@ -23,12 +23,22 @@ public class SignInActivity extends AppCompatActivity {
         setContentView(R.layout.activity_sign_in);
 
         initialize();
+        createErrorListener();
         createClickListener();
-
     }
+
+
 
     private void createClickListener() {
         sign_in_back_imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                intent = new Intent(getBaseContext(), WelcomePageActivity.class);
+                finish();
+            }
+        });
+
+        sign_in_sign_in_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 intent = new Intent(getBaseContext(), WelcomePageActivity.class);
@@ -36,6 +46,10 @@ public class SignInActivity extends AppCompatActivity {
                 finish();
             }
         });
+    }
+
+    private void createErrorListener() {
+        
     }
 
     private void initialize() {
