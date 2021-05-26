91128d3a4b2cfccc70671da33eb98311b1d203e1
Bryan <bryanmuliawan@yahoo.co.id>
2021-05-26 21:20:35 +0700

    Create Bottom Navigation

---
+++ app/src/main/java/com/snap/eonmemory/HomeActivity.java
@@ -0,0 +1,67 @@
+package com.snap.eonmemory;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class HomeActivity extends AppCompatActivity {
+
+    private BottomNavigationView home_bottomNavigation;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        initView();
+        setListener();
+    }
+
+    private void setFragment(Fragment fragment) {
+        FragmentTransaction fm = getSupportFragmentManager().beginTransaction();
+        Fragment currentFragment = getSupportFragmentManager().findFragmentById(R.id.home_fragmentContainer);
+
+        if (currentFragment == null) {
+            fm.add(R.id.home_fragmentContainer, fragment).commit();
+        } else {
+            fm.replace(R.id.home_fragmentContainer, fragment).commit();
+        }
+
+    }
+
+    private void setListener() {
+        home_bottomNavigation.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment fragment = null;
+
+                switch (item.getItemId()) {
+                    case R.id.menu_task:
+                        fragment = new TaskFragment();
+                        break;
+                    case R.id.menu_calendar:
+                        fragment = new CalendarFragment();
+                        break;
+                    case R.id.menu_note:
+                        fragment = new NoteFragment();
+                        break;
+                }
+
+                setFragment(fragment);
+
+                return fragment != null;
+            }
+        });
+    }
+
+    private void initView() {
+        home_bottomNavigation = findViewById(R.id.home_bottomNavigation);
+        setFragment(new TaskFragment());
+    }
+}
---
+++ app/src/main/java/com/snap/eonmemory/TaskFragment.java
@@ -0,0 +1,21 @@
+package com.snap.eonmemory;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class TaskFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_task, container, false);
+
+        return view;
+    }
+}
