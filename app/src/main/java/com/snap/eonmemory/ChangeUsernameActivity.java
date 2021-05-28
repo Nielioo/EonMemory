00cc6e63d741b240d9d441d9e3133951eab4e593
Daniel Aprillio <daprillio@student.ciputra.ac.id>
2021-05-28 14:40:03 +0700

    ChangeUsernameActivity design layout

---
+++ app/src/main/java/com/snap/eonmemory/ChangeUsernameActivity.java
@@ -0,0 +1,14 @@
+package com.snap.eonmemory;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ChangeUsernameActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_change_username);
+    }
+}
