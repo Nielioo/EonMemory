2b4a073c5b5e60e69e90957afb8a386608faa3e1
Bryan <bryanmuliawan@yahoo.co.id>
2021-05-30 03:29:54 +0700

    Add swipe to refresh

--- app/src/main/java/com/snap/eonmemory/TaskFragment.java
+++ app/src/main/java/com/snap/eonmemory/TaskFragment.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -17,6 +18,7 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -30,6 +32,8 @@ public class TaskFragment extends Fragment {
 
     private View view;
     private RecyclerView home_recyclerView_task;
+    private SwipeRefreshLayout task_swipeRefresh;
+    private FloatingActionButton task_FAB_create;
     private ArrayList<Task> taskList;
     private TaskRVAdapter adapter;
 
@@ -42,11 +46,37 @@ public class TaskFragment extends Fragment {
         initView();
         setRecyclerView();
         loadDataDB();
+        setListener();
+        setSwipeRefresh();
 
         return view;
     }
 
+    private void setSwipeRefresh() {
+        task_swipeRefresh.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                loadDataDB();
+
+                task_swipeRefresh.setRefreshing(false);
+            }
+        });
+    }
+
+    private void setListener() {
+        task_FAB_create.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                CreateTaskFragment createTask = new CreateTaskFragment();
+                createTask.show(getParentFragmentManager(), null);
+            }
+        });
+    }
+
     private void loadDataDB() {
+        // Clear arraylist to prevent same data to be loaded twice
+        taskList.clear();
+
         // Localhost
         String url = "http://192.168.1.6/EonMemory/EonMemoryDB/ReadAllTask.php";
 
@@ -99,6 +129,8 @@ public class TaskFragment extends Fragment {
 
     private void initView() {
         home_recyclerView_task = view.findViewById(R.id.home_recyclerView_task);
+        task_swipeRefresh = view.findViewById(R.id.task_swipeRefresh);
+        task_FAB_create = view.findViewById(R.id.task_FAB_create);
         taskList = new ArrayList<Task>();
         adapter = new TaskRVAdapter(taskList); // Add card listener later
     }
