b7c94a873b0ee72347a1256b76974a4f9b3d81f4
Bryan <bryanmuliawan@yahoo.co.id>
2021-06-08 22:31:51 +0700

    Update setting page

--- app/src/main/java/com/snap/eonmemory/SettingPageActivity.java
+++ app/src/main/java/com/snap/eonmemory/SettingPageActivity.java
@@ -3,12 +3,32 @@ package com.snap.eonmemory;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class SettingPageActivity extends AppCompatActivity {
+
+    private ImageView setting_imageView_back;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting_page);
+
+        initView();
+        setListener();
+    }
+
+    private void setListener() {
+        setting_imageView_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+
+    private void initView() {
+        setting_imageView_back = findViewById(R.id.setting_imageView_back);
     }
 }
